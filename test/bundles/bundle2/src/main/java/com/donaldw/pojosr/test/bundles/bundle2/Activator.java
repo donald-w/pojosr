048f4fc912d754470bfff604c2123c6662a13b18
Donald W <github@donaldw.com>
2015-12-21 23:16:13 +0000

    More maven hubris

--- test/bundles/bundle2/src/main/java/com/donaldw/pojosr/test/bundles/bundle2/Activator.java
+++ test/bundles/bundle2/src/main/java/com/donaldw/pojosr/test/bundles/bundle2/Activator.java
@@ -2,18 +2,23 @@ package com.donaldw.pojosr.test.bundles.bundle2;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author donald-w
  */
 public class Activator implements BundleActivator {
+    private Logger logger = LoggerFactory.getLogger(Activator.class);
+
     @Override
     public void start(BundleContext context) throws Exception {
-        System.out.println("Starting Bundle 2");
+        logger.info("Starting Bundle 2");
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
-        System.out.println("Stopping Bundle 2");
+        logger.info("Stopping Bundle 2");
     }
 }
+
--- test/bundles/bundle3/src/main/java/com/donaldw/pojosr/test/bundles/bundle3/Activator.java
+++ test/bundles/bundle3/src/main/java/com/donaldw/pojosr/test/bundles/bundle3/Activator.java
@@ -2,18 +2,23 @@ package com.donaldw.pojosr.test.bundles.bundle3;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author donald-w
  */
 public class Activator implements BundleActivator {
+    private Logger logger = LoggerFactory.getLogger(Activator.class);
+
     @Override
     public void start(BundleContext context) throws Exception {
-        System.out.println("Starting Bundle 3");
+        logger.info("Starting Bundle 3");
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
-        System.out.println("Stopping Bundle 3");
+        logger.info("Stopping Bundle 3");
     }
 }
+
