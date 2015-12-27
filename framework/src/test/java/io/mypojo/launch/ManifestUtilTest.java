44ee864f4dac61825c15e653ddc052e170e2687d
Donald W <github@donaldw.com>
2015-12-27 22:13:52 +0000

    Update copyright notices

--- framework/src/test/java/io/mypojo/launch/ManifestUtilTest.java
+++ framework/src/test/java/io/mypojo/launch/ManifestUtilTest.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2015 Donald W - github@donaldw.com
+ * Copyright 2011 Karl Pauls karlpauls@gmail.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
