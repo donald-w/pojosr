7abfa1ead6eb6550685536cb89136be1a3ecadb4
Donald W <github@donaldw.com>
2016-05-21 21:49:35 +0100

    Merge ClassPathResources and JarResources

--- jcl/src/main/java/io/mypojo/jcl/ClasspathResources.java
+++ jcl/src/main/java/io/mypojo/jcl/ClasspathResources.java
@@ -22,8 +22,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarInputStream;
+import java.util.jar.Manifest;
 
 
 /**
@@ -32,13 +39,15 @@ import java.net.URL;
  *
  * @author Kamran Zafar
  */
-public class ClasspathResources extends JarResources {
+public class ClasspathResources {
 
-    private static Logger logger = LoggerFactory.getLogger(ClasspathResources.class);
+    private static final Logger logger = LoggerFactory.getLogger(ClasspathResources.class);
+    private final Map<String, JclJarEntry> jarEntryContents = new HashMap<>();
+    protected boolean collisionAllowed;
     private boolean ignoreMissingResources;
 
     public ClasspathResources() {
-        super();
+        collisionAllowed = Configuration.suppressCollisionException();
         ignoreMissingResources = Configuration.suppressMissingResourceException();
     }
 
@@ -251,6 +260,183 @@ public class ClasspathResources extends JarResources {
         } else {
             throw new ResourceNotFoundException(resource, "Resource not found in local ClasspathResources");
         }
+    }
+
+    public URL getResourceURL(String name) {
+
+        JclJarEntry entry = jarEntryContents.get(name);
+        if (entry != null) {
+            if (entry.getBaseUrl() == null) {
+                throw new JclException("non-URL accessible resource");
+            }
+            try {
+                return new URL(entry.getBaseUrl() + name);
+            } catch (MalformedURLException e) {
+                throw new JclException(e);
+            }
+        }
+
+        return null;
+    }
+
+    public byte[] getResource(String name) {
+        JclJarEntry entry = jarEntryContents.get(name);
+        if (entry != null) {
+            return entry.getResourceBytes();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Returns an immutable Map of all jar resources
+     *
+     * @return Map
+     */
+    public Map<String, byte[]> getResources() {
+
+        Map<String, byte[]> resourcesAsBytes = new HashMap<>(jarEntryContents.size());
+
+        for (Map.Entry<String, JclJarEntry> entry : jarEntryContents.entrySet()) {
+            resourcesAsBytes.put(entry.getKey(), entry.getValue().getResourceBytes());
+        }
+
+        return resourcesAsBytes;
+    }
+
+    /**
+     * Reads the specified jar file
+     */
+    public void loadJar(String jarFile) {
+        if (logger.isDebugEnabled())
+            logger.debug("Loading jar: " + jarFile);
+
+        File file = new File(jarFile);
+        String baseUrl = "jar:" + file.toURI().toString() + "!/";
+
+        try (FileInputStream fis = new FileInputStream(file)) {
+            loadJar(baseUrl, fis);
+        } catch (IOException e) {
+            throw new JclException(e);
+        }
+    }
+
+    /**
+     * Reads the jar file from a specified URL
+     */
+    public void loadJar(URL url) {
+        if (logger.isDebugEnabled())
+            logger.debug("Loading jar: " + url.toString());
+
+        try (InputStream in = url.openStream()) {
+            String baseUrl = "jar:" + url.toString() + "!/";
+            loadJar(baseUrl, in);
+        } catch (IOException e) {
+            throw new JclException(e);
+        }
+    }
+
+    /**
+     * Load the jar contents from InputStream
+     */
+    public void loadJar(String argBaseUrl, InputStream jarStream) {
+
+        try (
+                BufferedInputStream bis = new BufferedInputStream(jarStream);
+                JarInputStream jis = new JarInputStream(bis)
+        )
+            {
+
+            JarEntry jarEntry;
+            while ((jarEntry = jis.getNextJarEntry()) != null) {
+                if (logger.isDebugEnabled())
+                    logger.debug(dump(jarEntry));
+
+                if (jarEntry.isDirectory()) {
+                    continue;
+                }
+
+                if (jarEntryContents.containsKey(jarEntry.getName())) {
+                    if (!collisionAllowed)
+                        throw new JclException("Class/Resource " + jarEntry.getName() + " already loaded");
+                    else {
+                        if (logger.isDebugEnabled())
+                            logger.debug("Class/Resource " + jarEntry.getName()
+                                    + " already loaded; ignoring entry...");
+                        continue;
+                    }
+                }
+
+                if (logger.isDebugEnabled())
+                    logger.debug("Entry Name: " + jarEntry.getName() + ", " + "Entry Size: " + jarEntry.getSize());
+
+                byte[] b = new byte[2048];
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+                int len;
+                while ((len = jis.read(b)) > 0) {
+                    out.write(b, 0, len);
+                }
+
+                // add to internal resource HashMap
+                JclJarEntry entry = new JclJarEntry();
+                entry.setBaseUrl(argBaseUrl);
+                entry.setResourceBytes(out.toByteArray());
+                jarEntryContents.put(jarEntry.getName(), entry);
+
+                if (logger.isDebugEnabled())
+                    logger.debug(jarEntry.getName() + ": size=" + out.size() + " ,csize="
+                            + jarEntry.getCompressedSize());
+
+                out.close();
+            }
+
+            // JarInputStream 'consumes' the manifest. We want it available, since it fundamentally underpins how the ClassPathScanner works
+            Manifest manifest = jis.getManifest();
+            if (manifest != null) {
+                try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+                    manifest.write(out);
+
+                    JclJarEntry entry = new JclJarEntry();
+                    entry.setBaseUrl(argBaseUrl);
+                    entry.setResourceBytes(out.toByteArray());
+                    jarEntryContents.put(JarFile.MANIFEST_NAME, entry);
+                }
+            }
+
+        } catch (IOException e) {
+            throw new JclException(e);
+        } catch (NullPointerException e) {
+            if (logger.isDebugEnabled())
+                logger.debug("Done loading.");
+        }
+    }
+
+    /**
+     * For debugging
+     */
+    private String dump(JarEntry je) {
+        StringBuilder sb = new StringBuilder();
+        if (je.isDirectory()) {
+            sb.append("d ");
+        } else {
+            sb.append("f ");
+        }
+
+        if (je.getMethod() == JarEntry.STORED) {
+            sb.append("stored   ");
+        } else {
+            sb.append("defalted ");
+        }
+
+        sb.append(je.getName());
+        sb.append("\t");
+        sb.append("").append(je.getSize());
+        if (je.getMethod() == JarEntry.DEFLATED) {
+            sb.append("/").append(je.getCompressedSize());
+        }
+
+        return (sb.toString());
     }
 
     public boolean isCollisionAllowed() {
